353f45f97e08e22c7369fbd9b483147a795eccd7
Saeed RayatMoghadam <s.rayatmoghadam@gmail.com>
2022-03-28 18:28:59 +0430

    Add Service and Repository Interface , CourseViewModel

---
+++ CleanArchDemo/CleanArchDemo.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CleanArchDemo.Application.ViewModels;
+
+namespace CleanArchDemo.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
---
+++ CleanArchDemo/CleanArchDemo.Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchDemo.Domain.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CleanArchDemo.Application.ViewModels
+{
+    public class CourseViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
