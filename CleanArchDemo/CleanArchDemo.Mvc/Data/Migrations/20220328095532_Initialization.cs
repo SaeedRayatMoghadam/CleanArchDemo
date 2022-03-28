2d8beb31573228cbb7e524b43ca4fb0aa313fa36
Saeed RayatMoghadam <s.rayatmoghadam@gmail.com>
2022-03-28 14:26:34 +0430

    Add MVC Project

---
+++ CleanArchDemo/CleanArchDemo.Mvc/Data/Migrations/20220328095532_Initialization.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchDemo.Mvc.Data.Migrations
+{
+    public partial class Initialization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
