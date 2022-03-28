f416bc3757c3a8e5effd4ae505980780711aa68b
Saeed RayatMoghadam <s.rayatmoghadam@gmail.com>
2022-03-29 00:37:03 +0430

    Commit changes for Presentation Course , Controller , Views and Authentication

---
+++ CleanArchDemo/CleanArchDemo.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchDemo.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchDemo.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            var courses = _courseService.GetCourses();
+            return View(courses);
+        }
+    }
+}
