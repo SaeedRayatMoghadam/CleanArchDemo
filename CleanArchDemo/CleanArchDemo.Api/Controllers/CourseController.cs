3c27862879b1cc1224e90a909cbed5320d656acb
Saeed RayatMoghadam <s.rayatmoghadam@gmail.com>
2022-03-29 19:47:48 +0430

    Add API Project and Create CourseController

---
+++ CleanArchDemo/CleanArchDemo.Api/Controllers/CourseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchDemo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+    }
+}
